593a3a126273e0fa22cc608c3e0e5a28ec002cad
chandrashekhar <cspandit123@gmail.com>
2022-11-02 01:52:16 -0400

    new changes

--- test.py
+++ test.py
@@ -1,2 +1,32 @@
-print("hello chandra")
+import functools
+import logging
+import time
 
+
+def decorated_by(decorated):
+    @functools.wraps(decorated)
+    def inner(*args, **kwargs):
+        inp = [i for i in kwargs.values()]
+        for x in list(args) + inp:
+            if not isinstance(x, int):
+                raise TypeError('%s function only except integers' % decorated.__name__)
+
+        start = time.time()
+        x = decorated(*args, **kwargs)
+        end = time.time()
+        delt = end - start
+
+
+        logging.basicConfig(level=logging.DEBUG, filename='test.log', filemode='w',
+                            format='%(asctime)s :  %(process)d - %(name)s - %(levelname)s - %(message)s')
+        logger = logging.getLogger("decorator logging")
+        logger.info("%s took %.02f" % (decorated.__name__, delt))
+        return x
+    return inner
+
+@decorated_by
+def add(x, y):
+    time.sleep(2)
+    return x+y
+
+print(add('',3))
