ff228f5837c6cc2fa1d3ddb8fc6d14bb327941ea
cpandit <cpandit@enphaseenergy.com>
2025-08-05 17:54:49 +0545

    Implement your own iterator

---
+++ Tutorials/Python/Custom_Iterator.py
@@ -0,0 +1,22 @@
+class MyIterator:
+    def __init__(self, iterable_object):
+        self.iterable_object = iterable_object
+        self.counter = 0
+        self.max = len(self.iterable_object)-1
+
+    def __iter__(self):
+        self
+
+    def __next__(self):
+        while self.counter <= self.max:
+            self.counter += 1
+            return self.iterable_object[self.counter-1]
+        if self.counter > self.max:
+            raise StopIteration
+
+
+x = 'chandra'
+
+y = MyIterator(x)
+print(y.__next__())
+print(next(y))
