ff7801328bf96d89792bdef486416368b259cf22
cpandit <cpandit@enphaseenergy.com>
2025-08-07 12:21:04 +0545

    Added deleted code

--- string/is_palindromable.py
+++ string/is_palindromable.py
@@ -1,1 +1,29 @@
 # if odd number of characters in a string more than one then it cannot form palindrome
+
+
+def palindrome_funny(s):
+    d = dict.fromkeys(s, 0)
+    for x in s:
+        d[x] += 1
+    count = 0
+    for i in d.values():
+        if i%2 != 0:
+            count += 1
+        if count > 1:
+            return False
+    return True
+
+
+def palindrome(s):
+    l = 0
+    h = len(s)-1
+    while l< h:
+        if s[l] != s[h]:
+            return False
+        l += 1
+        h -= 1
+    return True
+
+
+s = "malayalam"
+print(palindrome(s))
