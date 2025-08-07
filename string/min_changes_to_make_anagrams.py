393e5bcebf5c4c0de2ecafc30041c0e1312ca19e
cpandit <cpandit@enphaseenergy.com>
2025-08-07 14:31:30 +0545

    Added descriptions to for the problem

--- string/min_changes_to_make_anagrams.py
+++ string/min_changes_to_make_anagrams.py
@@ -1,4 +1,18 @@
-# minimum number to make two string anagrams
+"""
+You are given two strings s and t. In one step, you can append any character to either s or t.
+Return the minimum number of steps to make s and t anagrams of each other.
+An anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+Example :
+Input: s = "leetcode", t = "coats"
+Output: 7
+Explanation:
+- In 2 steps, we can append the letters in "as" onto s = "leetcode", forming s = "leetcodeas".
+- In 5 steps, we can append the letters in "leede" onto t = "coats", forming t = "coatsleede".
+"leetcodeas" and "coatsleede" are now anagrams of each other.
+We used a total of 2 + 5 = 7 steps.
+It can be shown that there is no way to make them anagrams of each other with less than 7 steps.
+"""
 
 def make_anagrams(s1, s2):
     d1 = dict.fromkeys(s1, 0)
@@ -15,7 +29,7 @@ def make_anagrams(s1, s2):
     print(d1)
     for x in d1.keys():
         count += abs(d1[x])
-    print(count//2)
+    print(count)
 
 def make_anagrams_array(s1, s2):
     A = [0]*26
@@ -29,9 +43,9 @@ def make_anagrams_array(s1, s2):
     for x in A:
         count += abs(x)
 
-    print(count//2)
+    print(count)
 
 
-s1 = "ddcf"
-s2 = "cedk"
-make_anagrams_array(s1, s2)
+s1 = "leetcode"
+s2 = "coats"
+make_anagrams(s1, s2)
