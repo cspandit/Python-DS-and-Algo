52c88219f31dc9d3d29f5db2f4deab7fc40b6dfc
shekhar pandit <cspandit123@gmial.com>
2025-08-08 08:44:07 +0545

    Added more efficient methods to super reduced string

--- string/super_reduced_string.py
+++ string/super_reduced_string.py
@@ -16,4 +16,32 @@ def superReducedString(s):
     else:
         return "".join(s_list)
 
-print(superReducedString('aaabccddd'))
+#using dictionary
+def superReducedString_chandra(s):
+    res = ''
+    d = dict.fromkeys(s, 0)
+    for c in s:
+        d[c] += 1
+    for c in s:
+        if d[c] % 2 != 0:
+            res += c
+            d[c] -= 1
+    return res
+
+#withoiut using dictionary extra list
+def superReducedString_chandra_string(s):
+    res = ''
+    n = len(s)
+    i = 0
+    while i < n:
+        if i == n-1:
+            res += s[i]
+            i += 1
+        elif s[i] ==  s[i+1]:
+            i += 2
+        else:
+            res += s[i]
+            i += 1
+    return res
+
+print(superReducedString_chandra_string('aaabccddd'))
