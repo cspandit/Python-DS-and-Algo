85657eb7aa65fb3df4ae2ec577e621894c3fcc48
cpandit <cpandit@enphaseenergy.com>
2025-08-11 13:19:17 +0545

    New pattern problem: print Z of nxn size

---
+++ Printing_Patterns/pattern7_printZ.py
@@ -0,0 +1,28 @@
+"""
+n=6
+1 2 3 4 5 6
+        7
+      8
+    9
+  0
+1 2 3 4 5 6
+"""
+
+
+def print_z(n):
+    count = 1
+    for i in range(n):
+        count = count % 10
+        for j in range(n):
+            if i == 0 or i == n - 1:
+                print(count, end=" ")
+                count += 1
+            elif j == n - i - 1:
+                print(count, end=" ")
+                count += 1
+            else:
+                print(" ", end=" ")
+        print('\r')
+
+
+print_z(6)
