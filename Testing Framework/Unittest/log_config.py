a15ac33dbcb97a2aa21b94b5e7178f2680789e08
shekhar pandit <cspandit123@gmial.com>
2025-08-17 21:34:18 +0545

    change in loggine

--- Testing Framework/Unittest/log_config.py
+++ Testing Framework/Unittest/log_config.py
@@ -1,26 +1,25 @@
 import logging
 from pathlib import Path
 from datetime import datetime
-from venv import logger
+# from venv import logger
 
 
 def setup_log(name):
     log_dir = Path('logs')
     log_dir.mkdir(exist_ok=True)
 
-    lgger = logging.getLogger()
+    lgger = logging.getLogger(name)
     lgger.setLevel(logging.DEBUG)
 
     time_stamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    file_handler = logging.FileHandler("logs/Sanity_Test_log_{}".format(time_stamp))
+    file_handler = logging.FileHandler("logs/{}_{}".format(name, time_stamp))
     file_handler.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s]:%(lineno)d %(message)s",
-                                  datefmt="%Y-%m-%d %H:%M:%S"
-                                  )
+    formatter = logging.Formatter(fmt="%(asctime)s [%(levelname)s]:%(lineno)d %(message)s",
+                                  datefmt="%Y-%m-%d %H:%M:%S")
 
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
@@ -29,4 +28,4 @@ def setup_log(name):
         lgger.addHandler(file_handler)
         lgger.addHandler(console_handler)
 
-    return logger
+    return lgger
