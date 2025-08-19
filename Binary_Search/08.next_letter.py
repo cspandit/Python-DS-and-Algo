affd588eba8d8fef6a25e914618792c0b604779d
shekhar pandit <cspandit123@gmial.com>
2025-08-19 09:57:52 +0545

    Numbering of binary array problem

--- Binary_Search/08.next_letter.py
+++ Binary_Search/08.next_letter.py
@@ -1,6 +1,6 @@
-# Problem is to find the next letter for given in a sorted alphabetical array
+# Problem is to find the next letter for given letter in a sorted alphabetical array
 # This problem is identical to finding ceil of given number in sorted array. Only difference is we return number if
-# found in array but here we dont.
+# found in array but here we don't.
 
 
 def next_letter(array, key):
@@ -22,6 +22,6 @@ def next_letter(array, key):
     return nex
 
 
-A = ['a', 'c', 'f', 'h', 'g', 'z']
+A = ['a', 'c', 'f', 'g', 'h', 'z']
 nex_let = next_letter(A, 'e')
 print(nex_let)
