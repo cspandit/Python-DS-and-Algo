1ca97f503449302a64f4bca42506eae591cd766b
cpandit <cpandit@enphaseenergy.com>
2025-09-10 09:31:58 +0530

    fixed bug

--- Binary_Search/08.next_letter.py
+++ Binary_Search/08.next_letter.py
@@ -6,16 +6,12 @@
 def next_letter(array, key):
     low = 0
     high = len(array)-1
-    nex = ""
+    nex = array[0]
     while low <= high:
         mid = low + (high-low)//2
-        if array[mid] == key:
-            low = mid + 1
-
-        elif array[mid] > key:
-            nex = max(nex, array[mid])
+        if array[mid] > key:
+            nex = array[mid]
             high = mid - 1
-
         else:
             low = mid + 1
 
